dd254bda302339c2ddaa2bc5d78bf8a9b98c8ddf
Eugene Timoshenko <Eugene.Timoshenko@dataart.com>
2024-10-24 20:15:58 +0300

    Fixed styles for header and catalog page. Added uuid library for generating unique ids.

--- src/components/CampersList/CampersList.jsx
+++ src/components/CampersList/CampersList.jsx
@@ -1,11 +1,12 @@
 import Camper from "../Camper/Camper";
 import style from "./CampersList.module.css";
+import { v4 as uuidv4 } from 'uuid';
 
 const CampersList = ({ campers }) => {
   return (
     <ul className={style.campersList}>
       {campers.map((camper) => (
-        <li key={camper.id}>
+        <li key={uuidv4()}>
           <Camper camper={camper} />
         </li>
       ))}
